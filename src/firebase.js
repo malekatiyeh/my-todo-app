88725e5f6c706dda52d3fcfa79e905e448c79214
malekatiyeh <malekf@atiyeh.es>
2026-04-21 17:05:59 -0400

    Fix formatting issues in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBgbWLHVrBZlHd28fftUPin4UT4xZnEyk8",
-    authDomain: "my-todo-app-60d9c.firebaseapp.com",
-    projectId: "my-todo-app-60d9c",
-    storageBucket: "my-todo-app-60d9c.firebasestorage.app",
-    messagingSenderId: "93494051094",
-    appId: "1:93494051094:web:602096a1c48b54f31ad037"
+  apiKey: "AIzaSyBgbWLHVrBZlHd28fftUPin4UT4xZnEyk8",
+  authDomain: "my-todo-app-60d9c.firebaseapp.com",
+  projectId: "my-todo-app-60d9c",
+  storageBucket: "my-todo-app-60d9c.firebasestorage.app",
+  messagingSenderId: "93494051094",
+  appId: "1:93494051094:web:602096a1c48b54f31ad037"
 };
 
 const app = initializeApp(firebaseConfig);
